7ea68c7268611cf5c8ff32388c8f10b04de5d352
shishin.sho <shishin.sho@bitflyer.com>
2020-04-03 15:34:55 +0900

    purify all unused css

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,11 +3,18 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+const path = require('path');
+const glob = require('glob');
+const PurifyCssWebpack = require('purifycss-webpack');
+
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new CleanWebpackPlugin({
             cleanAfterEveryBuildPatterns: ['dist']
+        }),
+        new PurifyCssWebpack({
+            paths: glob.sync(path.join(__dirname, 'src/*.html')) // purify all unused style
         })
     ]
 })
